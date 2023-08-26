c166993dc9f6d8596c0ff0f57eeecc102819f547
okobenoko <77877214+okobenoko@users.noreply.github.com>
2023-08-26 13:26:02 +0800

    Add files via upload

---
+++ lnGtkHeadFileOnArchlinux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo ln -s /usr/include/glib-2.0/* /usr/include/
+sudo ln -s /usr/lib/glib-2.0/include/glibconfig.h /usr/include
+sudo ln -s /usr/include/gtk-4.0/* /usr/include/
+sudo ln -s  /usr/include/cairo/* /usr/include
+sudo ln -s /usr/include/pango-1.0/pango /usr/include/pango
+sudo ln -s /usr/include/harfbuzz/* /usr/include/
+sudo ln -s /usr/include/gdk-pixbuf-2.0/gdk-pixbuf /usr/include/gdk-pixbuf
+sudo ln -s /usr/include/graphene-1.0/* /usr/include/
+sudo ln -s /usr/lib/graphene-1.0/include/graphene-config.h /usr/include/graphene-config.h
